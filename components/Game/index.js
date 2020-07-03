c7c1f1d002f31e238f42d1e6dd5137c34de4340b
snkly <800680+snkly@users.noreply.github.com>
2020-07-03 12:28:03 -0700

    Init commit

---
+++ components/Game/index.js
@@ -0,0 +1,65 @@
+import { useReducer, useState, useEffect } from "react";
+
+export default function Game() {
+  let initialState = {
+    gameState: 'NOT_STARTED', // In progress, 
+    score: 0,
+  }
+
+  let [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "START_GAME": {
+        return { ...state, gameState: "STARTED" };
+      }
+      case "END_GAME": {
+        return { ...state, gameState: "FINISHED" };
+      }
+      case "RESTART_GAME": {
+        return { ...state, gameState: "STARTED", score: 0};
+      }
+      default: {
+        throw new Error("What state?")
+      }
+    }
+  }, initialState); 
+
+  let { gameState, score } = state; 
+
+  return <>
+    {gameState === "NOT_STARTED" && (
+      <button onClick={() => {
+        dispatch({ type: "START_GAME" }); 
+      }}>
+        Start Game
+      </button>
+    )}
+    {gameState === "STARTED" && (
+      <button onClick={() => {
+        dispatch({ type: "END_GAME" }); 
+      }}>
+        End Game
+      </button>
+    )}
+    {gameState === "FINISHED" && (
+      <>
+        <div>Score: {score}</div>
+        <button onClick={() => {
+        dispatch({ type: "RESTART_GAME" }); 
+      }}>
+        Try again
+      </button>
+      </>
+    )}
+    <input onSubmit={(e) => console.log(e.target.value)} />
+    <style jsx>{`
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+  </>;
+};
---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Game from '@components/Game'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Typing game</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header />
+        <Game />
+      </main>
+
+      <Footer />
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
